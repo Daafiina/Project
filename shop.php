0ba7cdac7085fd31203a1c3597def9cf59956d5b
Artina Bektashi <ab51034@ubt-uni.net>
2022-04-23 18:13:49 +0200

    commits

--- shop.php
+++ shop.php
@@ -57,7 +57,7 @@
                 ?>
 
 
-        <div class="box">
+        <!-- <div class="box">
             <div class="icons">
                 <a href="#" class="fas fa-shopping-cart"></a>
                 <a href="#" class="fas fa-heart"></a>
@@ -231,7 +231,7 @@
                     <i class="far fa-star"></i>
                 </div>
             </div>
-        </div>
+        </div> -->
 
     </div>
 
